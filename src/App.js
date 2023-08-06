a510e7e2c87da78ea98f3362de2e4cf6dbc049b5
salome <sallygithinji72@gmail.com>
2023-08-06 21:09:14 +0300

    props

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2023, 3, 28),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2023, 3, 28)},
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2023, 3, 28),
     },
     {
       id: 'e4',
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,18 +1,17 @@
 import "./ExpenseItem.css"
 import React from "react";
 
-function ExpenseItem()
+function ExpenseItem(props)
 {
 
-const expenseDate = new Date(2023, 3, 28)
-const expenseTitle = "Car Insurance"
-const expenseAmount = "$234.55"
+// const expenseDate = new Date(2023, 3, 28)
+// const expenseTitle = "Car Insurance"
+// const expenseAmount = "$234.55"
     return (
         <div className="expense-item">
-    <div> {expenseDate.toDateString()}</div>
     <div className="expense-item__description">
-        <h2 >{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2 >{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
     </div>
         </div>
     
