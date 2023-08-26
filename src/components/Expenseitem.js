8a880e9fb997ada956c1fc316be9e09a3381cf02
salome <sallygithinji72@gmail.com>
2023-08-26 21:14:49 +0300

    more on usestate

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,25 +1,29 @@
 import "./ExpenseItem.css"
-import React from "react";
-import ExpenseDate  from "./ExpenseDate";
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
-function ExpenseItem({date,title,amount})
-{
+function ExpenseItem({ date, title: initialTitle, amount }) {
 
-const clickhandler = () => {
-alert("Button Clicked !!!")
-}
+    const [title, setTitle] = useState(initialTitle)
+    console.log("four instances of the component are created")
+
+    const clickHandler = () => {
+        setTitle("Updated")
+        console.log(initialTitle);
+
+    }
     return (
         <Card className="expense-item">
-        <ExpenseDate date = {date}/>   
-    <div className="expense-item__description">
-    <div className="expense-item__description">
-        <h2 >{title}</h2>
-        <div className="expense-item__price">${amount}</div>
-    </div>
-    </div>
-    <button onClick={clickhandler}>Change title</button>
+            <ExpenseDate date={date} />
+            <div className="expense-item__description">
+                <div className="expense-item__description">
+                    <h2 >{title}</h2>
+                    <div className="expense-item__price">${amount}</div>
+                </div>
+            </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
-    
+
     )
 }
 export default ExpenseItem;
