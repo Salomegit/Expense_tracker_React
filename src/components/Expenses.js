eac8da9d11d4b341998f4d71c6b557ada705df26
salome <sallygithinji72@gmail.com>
2023-10-13 22:55:23 +0300

    having two states related to one anoter

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -8,17 +8,33 @@ export default function Expenses({ sally }) {
 
 
   const [filteredYear, SetFilteredYear] = useState("2020")
-
+  const [otherYears, setOtheryears] = useState("2019,2021,2023")
 
   const ChangeFilter = (selectedYear) => {
-    console.log("expense.js")
-    console.log(selectedYear)
+
+
     SetFilteredYear(selectedYear)
+    if (selectedYear === "2020") {
+      setOtheryears("2019,2021,2023")
+    } else if (selectedYear === "2019") {
+      setOtheryears("2020,2021,2023")
+
+    }
+    else if (selectedYear === "2021") {
+      setOtheryears("2020,2021,2023")
+
+    } else if (selectedYear === "2023") {
+      setOtheryears("2020,2021,2023")
+
+    }
+    setOtheryears(otherYears)
   }
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeYearFilter={ChangeFilter} />
 
+      <ExpensesFilter selected={filteredYear} onChangeYearFilter={ChangeFilter}
+      />
+      <p>hidden years are {otherYears}</p>
 
       {sally.map((expense) => (
         <ExpenseItem
